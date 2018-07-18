0e94da154563359ac45c8530cc8ee609c82dddb3
Vpedrotra <victorpedrota@hotmail.com>
2018-07-18 02:20:36 -0300

    arquivos inuteis

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ if(!isset($_SESSION["system_control"]))
     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img class="d-block w-100" src="imagens/velocimetro1.jpg" alt="First slide">
+          <img class="d-block w-100" src="imagens/velocimetro2.jpg" alt="First slide">
         </div>
         <div class="carousel-item">
           <img class="d-block w-100" src="imagens/carro.jpg" alt="Second slide">
@@ -122,14 +122,14 @@ if(!isset($_SESSION["system_control"]))
   </section>
 
   <section class="action-sec">
+    <center>
     <div class="container">
       <a href="#"><img src="imagens/facebook.png" width="45" height="45"></a>
       <a href="#"><img src="imagens/Google.png" width="45" height="45"></a>
       <a href="#"><img src="imagens/instagram.png" width="45" height="45"></a>
       <a href="#"><img src="imagens/youtube.png" width="45" height="45"></a>
       <a href="#"><img src="imagens/whatsapp.png" width="45" height="45"></a>
-      (12) 99999-9999
-    </div>
+    </div></center>
   </section>
 </body>
 </html>
