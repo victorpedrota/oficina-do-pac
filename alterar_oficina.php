5464d0bdb7e392bc7402aa71158c2ed08ceeec59
pepelindo <41274007+pepelindo@users.noreply.github.com>
2018-11-18 16:36:07 -0200

    f

--- alterar_oficina.php
+++ alterar_oficina.php
@@ -49,7 +49,7 @@ if (isset ($_POST['senha_nova'])) {
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Senha alterada com sucesso</h5>
+        <h5 class="modal-title" id="exampleModalLongTitle">Senha alterada com sucesso!</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
